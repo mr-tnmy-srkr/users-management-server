22dbd1d83c6b772c4a8366a12eda1baef14b7429
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-12 23:51:19 +0530

    module complete

--- users-management-server/index.js
+++ users-management-server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-
+app.use(cors());
+app.use(express.json());
 
 const users = [
   {
@@ -33,10 +33,16 @@ app.get("/users", (req, res) => {
   res.send(users);
 });
 
+
+//client side theke data asbe abar client side e chole jabe
 app.post("/users", (req, res) => {
-  console.log('post api hitting');
-console.log(req.body);
-})
+  console.log("post api hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
